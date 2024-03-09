b2ff9144d7c5ac0b8368a8293d7b4a74e1e3e1da
Tejasva Jaitly <tejasvajaitly@gmail.com>
2024-03-09 01:31:33 -0800

    initialize express + ts app

---
+++ index.ts
@@ -0,0 +1,15 @@
+import express from "express"
+
+import 'dotenv/config'
+
+const app = express()
+
+const port  = process.env.PORT
+
+app.get('/', (req, res) => {
+    res.send("Hello World")
+})
+
+app.listen(port, () => {
+    console.log(`chordsAI-back-end listening on port ${port}`)
+})
